2b58fb6ab819c57a236f8301df444ffd071f148a
emil brunner <emil.brunner@gmx.de>
2019-05-21 21:53:57 +0200

    update pi-update.sh

--- raspberrypi/pi-update.sh
+++ raspberrypi/pi-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo Betriebssystem wird aktualisiert!
-sudo apt update && sudo apt upgrade && sudo apt -y autoremove
+sudo apt -y update && sudo apt -y upgrade && sudo apt -y autoremove
 
