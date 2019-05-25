26591e8e51a9f3a3f89a7b89c86c10b41adf0835
Emil <emil.brunner@gmx.de>
2019-05-25 19:38:21 +0200

    .

--- raspberrypi/pi-update.sh
+++ raspberrypi/pi-update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo Betriebssystem wird aktualisiert!
+echo Betriebssystem wird aktualisiert...
 sudo apt -y update && sudo apt -y upgrade && sudo apt -y autoremove
 
+echo Betriebssystem aktualisiert!
